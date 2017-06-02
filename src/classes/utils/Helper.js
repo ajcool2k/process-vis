8a218508b735e1d324c164d669d58ef5b4d2db67
ajcool <jens.auerswald@gmail.com>
2017-06-02 12:54:32 +0200

    Functions to read and add scopeProps for dynamic content in vue

--- src/classes/utils/Helper.js
+++ src/classes/utils/Helper.js
@@ -21,4 +21,39 @@ export class Helper {
     let highestId = Math.max(...idList) + 1   // Math.max.apply(null, idList) + 1
     return highestId
   }
+
+  /**
+   * Methode liefert den Vue-Scoped-Prop
+   * @param {Node} elem Element das einen vue-Prop aufweist.
+   * @returns String oder null
+   */
+  static getScopeProp (elem) {
+    let propObj = elem.attributes
+
+    let scopedData = Object.keys(propObj).map(key => {
+      return propObj[key]
+    }).filter(elem => {
+      return elem.nodeName.startsWith('data-v-')
+    })
+
+    let scopedValue = scopedData.length > 0 ? scopedData[0].nodeName : null
+    return scopedValue
+  }
+
+  static addScopeProp (elem, scopedProp) {
+    var childList = elem.childNodes
+
+    if (childList.length === 0) {
+      // console.log(elem)
+      if (elem.nodeType !== 3) elem.setAttribute(scopedProp, '')
+      return
+    }
+
+    // console.log('addScopeProp - childs: ' + childList.length)
+
+    childList.forEach(function (child) {
+      Helper.addScopeProp(child, scopedProp)
+      if (child.nodeType !== 3) child.setAttribute(scopedProp, '')
+    }, this)
+  }
 }
