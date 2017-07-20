961ef904f7a9819517c31e8b9f2406a4150132c6
ajcool <jens.auerswald@gmail.com>
2017-07-20 12:40:32 +0200

    drop unchanged transitions as well

--- src/classes/utils/Animate.js
+++ src/classes/utils/Animate.js
@@ -1,47 +1,56 @@
 export class Animate {
-  static run (node, nodeId, triggerProp, style, time) {
-    // console.warn('Animate: ' + nodeId + '-' + triggerProp)
+  static start (node, handlerId, triggerProp, style, time) {
+    // update view
     node.style.transitionProperty = triggerProp
     node.style.transitionDuration = time + 's'
     node.style.transitionStyle = style
+
+    // timeout for unchanged dom elements
+    setTimeout(() => {
+      if (typeof Animate.handler[handlerId] !== 'undefined') Animate.handler[handlerId].fnAfter()
+    }, time * 2000)
   }
 
-  static afterTransition (node, handlerId, callback) {
+  static afterTransition (node, handlerId, fnAfter) {
+    if (!fnAfter || typeof fnAfter !== 'function') return
+
     // console.log('Animate: ' + handlerId + ' prepare')
     let fn = () => {
       // console.log('Animate: ' + handlerId + ' afterTransition start')
-      callback()
+      fnAfter()
       // console.log('Animate: ' + handlerId + ' afterTransition done')
-      Animate.removeCallbackListener(handlerId)
+      Animate.removeTransitionHandler(handlerId)
+       // console.log('Animate: remaining handler: ' + Object.keys(Animate.handler).length)
       Animate.clearAnimation(node, handlerId)
       // console.log('-------------------')
     }
 
-    node.removeEventListener('transitionend', fn, false)    // remove old first
-    node.addEventListener('transitionend', fn, false)       // add new listener
-    Animate.handler[handlerId] = { node: node, fn: fn }
-    // console.log('Animate: collection handler: ' + Object.keys(Animate.handler).length)
-    // console.log(Animate.handler)
+    Animate.addTransitionDoneHandler(node, handlerId, fn)
   }
 
-  static addCallbackListener (node, handlerId, handler) {
+  static addTransitionDoneHandler (node, handlerId, handler) {
+    node.removeEventListener('transitionend', handler, false)    // remove old first
     node.addEventListener('transitionend', handler, false)
-    Animate.handler[handlerId] = { node: node, fn: handler }
+    Animate.handler[handlerId] = typeof Animate.handler[handlerId] !== 'undefined' ? Animate.handler[handlerId] : {}
+    Animate.handler[handlerId].node = node
+    Animate.handler[handlerId].fnAfter = handler
+
     // console.log('Animate: remaining handler: ' + Object.keys(Animate.handler).length)
     // console.log(Animate.handler)
   }
 
-  static removeCallbackListener (handlerId) {
+  static removeTransitionHandler (handlerId) {
     let node = Animate.handler[handlerId].node
-    node.removeEventListener('transitionend', Animate.handler[handlerId].fn, false)
+    node.removeEventListener('transitionend', Animate.handler[handlerId].fnAfter, false)
     delete Animate.handler[handlerId]
+
     // console.log('Animate: ' + handlerId + ' callback removed')
     // console.log('Animate: remaining handler: ' + Object.keys(Animate.handler).length)
     // console.log(Animate.handler)
     return node
   }
 
-  static clearAnimation (node, animationName) {
+  static clearAnimation (node) {
     if (!node || typeof node === 'undefined') {
       console.warn('Animate: clearAnimation expected node, got undefined')
       return
@@ -50,16 +59,15 @@ export class Animate {
     node.style.transitionProperty = 'none'
     node.style.transitionDuration = 'initial'
     node.style.transitionStyle = 'none'
-    // console.log('Animate: ' + animationName + ' styling (transition) cleared')
   }
 
   static clear () {
     console.warn('Animate: clear()')
     for (let handlerId in Animate.handler) {
-      let node = Animate.removeCallbackListener(handlerId)
-      Animate.clearAnimation(node, handlerId)
+      let node = Animate.removeTransitionHandler(handlerId)
+      Animate.clearAnimation(node)
     }
-    // console.log(Animate.handler)
+    console.log(Animate)
   }
 }
 
