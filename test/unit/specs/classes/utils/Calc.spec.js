015212185f073e551ad6d5e41da29a8712e59ab9
ajcool <jens.auerswald@gmail.com>
2017-09-19 16:39:03 +0200

    added Unit-Tests

--- test/unit/specs/classes/utils/Calc.spec.js
+++ test/unit/specs/classes/utils/Calc.spec.js
@@ -213,3 +213,307 @@ describe('Calc.js (updateEndDate)', () => {
     expect(durationBefore).to.be.equal(durationAfter)
   })
 })
+
+describe('Calc.js (getDivider)', () => {
+  it('should return 1 for wrong format', () => {
+    expect(Calc.getDivider()).to.equal(1)
+    expect(Calc.getDivider('test')).to.equal(1)
+    expect(Calc.getDivider(null)).to.equal(1)
+  })
+
+  it('should return correct divider', () => {
+    expect(Object.keys(Calc.timeFormats).length).to.equal(3)
+    const formats = Object.keys(Calc.timeFormats)
+    formats.forEach(format => {
+      let divider = Calc.getDivider(format)
+      expect(divider).to.be.a('number')
+      expect(divider).to.be.above(0)
+      expect(divider).to.equal(Calc.timeFormats[format])
+    })
+  })
+})
+
+describe('Calc.js (addSpace)', () => {
+  it('should not add space', () => {
+    const participants = ['1', '2', '3', '4']
+
+    const childs = [
+      new Process('1', participants[0], new Date(2017, 0, 1)),
+      new Process('1', participants[1], new Date(2017, 0, 2)),
+      new Process('1', participants[2], new Date(2017, 0, 2))
+    ]
+
+    childs[0]._height = 100
+    childs[0]._position = { x: 0, y: 0 }
+
+    childs[1]._height = 100
+    childs[1]._position = { x: 0, y: 200 }
+
+    childs[2]._height = 100
+    childs[2]._position = { x: 0, y: 200 }
+
+    const clone = require('clone')
+    const childsCopy = clone(childs)
+
+    let wasSuccessful = Calc.addSpace(childs, Calc.timeSlice)
+    let isEqual = _.isEqual(childs, childsCopy)
+
+    expect(wasSuccessful).to.equal(true)
+    expect(isEqual).to.equal(true)
+  })
+
+  it('should add space to one', () => {
+    const participants = ['1', '2']
+
+    const childs = [
+      new Process('1', participants[0], new Date(2017, 0, 1)),
+      new Process('1', participants[1], new Date(2017, 0, 1))
+    ]
+
+    childs[0]._height = 100
+    childs[0]._position = { x: 0, y: 0 }
+
+    childs[1]._height = 100
+    childs[1]._position = { x: 0, y: 100 }
+
+    const clone = require('clone')
+    const childsCopy = clone(childs)
+
+    let wasSuccessful = Calc.addSpace(childs, Calc.timeSlice)
+    let isEqual = _.isEqual(childs, childsCopy)
+
+    expect(wasSuccessful).to.equal(true)
+    expect(isEqual).to.equal(false)
+
+    expect(childs[0]._position.y).to.equal(childsCopy[0]._position.y)
+    expect(childs[1]._position.y).to.equal(childsCopy[1]._position.y + Calc.timeSlice)
+  })
+
+  it('should add space to the first and second', () => {
+    const participants = ['1', '2', '3']
+
+    const childs = [
+      new Process('1', participants[0], new Date(2017, 0, 1)),
+      new Process('1', participants[1], new Date(2017, 0, 1)),
+      new Process('1', participants[2], new Date(2017, 0, 1))
+    ]
+
+    childs[0]._height = 100
+    childs[0]._position = { x: 0, y: 0 }
+
+    childs[1]._height = 100
+    childs[1]._position = { x: 0, y: 100 }
+
+    childs[2]._height = 100
+    childs[2]._position = { x: 0, y: 100 }
+
+    const clone = require('clone')
+    const childsCopy = clone(childs)
+
+    let wasSuccessful = Calc.addSpace(childs, Calc.timeSlice)
+    let isEqual = _.isEqual(childs, childsCopy)
+
+    expect(wasSuccessful).to.equal(true)
+    expect(isEqual).to.equal(false)
+
+    expect(childs[0]._position.y).to.equal(childsCopy[0]._position.y)
+    expect(childs[1]._position.y).to.equal(childsCopy[1]._position.y + Calc.timeSlice)
+    expect(childs[2]._position.y).to.equal(childsCopy[2]._position.y + Calc.timeSlice)
+  })
+
+  it('should add space to the first and twice to the second', () => {
+    const participants = ['1', '2', '3']
+
+    const childs = [
+      new Process('1', participants[0], new Date(2017, 0, 1)),
+      new Process('1', participants[1], new Date(2017, 0, 1)),
+      new Process('1', participants[2], new Date(2017, 0, 1))
+    ]
+
+    childs[0]._height = 100
+    childs[0]._position = { x: 0, y: 0 }
+
+    childs[1]._height = 100
+    childs[1]._position = { x: 0, y: 100 }
+
+    childs[2]._height = 100
+    childs[2]._position = { x: 0, y: 200 }
+
+    const clone = require('clone')
+    const childsCopy = clone(childs)
+
+    let wasSuccessful = Calc.addSpace(childs, Calc.timeSlice)
+    let isEqual = _.isEqual(childs, childsCopy)
+
+    expect(wasSuccessful).to.equal(true)
+    expect(isEqual).to.equal(false)
+
+    expect(childs[0]._position.y).to.equal(childsCopy[0]._position.y)
+    expect(childs[1]._position.y).to.equal(childsCopy[1]._position.y + Calc.timeSlice)
+    expect(childs[2]._position.y).to.equal(childsCopy[2]._position.y + Calc.timeSlice * 2)
+  })
+
+  it('should add space to the first and twice to the second and third', () => {
+    const participants = ['1', '2', '3']
+
+    const childs = [
+      new Process('1', participants[0], new Date(2017, 0, 1)),
+      new Process('1', participants[1], new Date(2017, 0, 2)),
+      new Process('1', participants[2], new Date(2017, 0, 3)),
+      new Process('1', participants[3], new Date(2017, 0, 3))
+    ]
+
+    childs[0]._height = 100
+    childs[0]._position = { x: 0, y: 0 }
+
+    childs[1]._height = 100
+    childs[1]._position = { x: 0, y: 100 }
+
+    childs[2]._height = 100
+    childs[2]._position = { x: 0, y: 200 }
+
+    childs[3]._height = 100
+    childs[3]._position = { x: 0, y: 200 }
+
+    const clone = require('clone')
+    const childsCopy = clone(childs)
+
+    let wasSuccessful = Calc.addSpace(childs, Calc.timeSlice)
+    let isEqual = _.isEqual(childs, childsCopy)
+
+    expect(wasSuccessful).to.equal(true)
+    expect(isEqual).to.equal(false)
+
+    expect(childs[0]._position.y).to.equal(childsCopy[0]._position.y)
+    expect(childs[1]._position.y).to.equal(childsCopy[1]._position.y + Calc.timeSlice)
+    expect(childs[2]._position.y).to.equal(childsCopy[2]._position.y + Calc.timeSlice * 2)
+    expect(childs[3]._position.y).to.equal(childsCopy[3]._position.y + Calc.timeSlice * 2)
+  })
+})
+
+describe('Calc.js (processPosition)', () => {
+  const clone = require('clone')
+
+  const participants = ['1', '2', '3', '4']
+  const childsUntouched = [
+    new Process('1', participants[0], new Date(2017, 0, 1)),
+    new Process('1', participants[1], new Date(2017, 0, 4)),
+    new Process('1', participants[2], new Date(2017, 0, 7)),
+    new Process('1', participants[3], new Date(2017, 0, 10))
+  ]
+
+  const containerSize = { x: Calc.minContainerWidth, y: Calc.minContainerHeight }
+  const timeFormat = 'days'
+
+  it('should not add private properties', () => {
+    const childs = clone(childsUntouched)
+    Calc.processPosition(undefined, childsUntouched, containerSize, timeFormat)
+    expect(_.isEqual(childs, childsUntouched)).to.equal(true)
+    Calc.processPosition(childs, undefined, containerSize, timeFormat)
+    expect(_.isEqual(childs, childsUntouched)).to.equal(true)
+    Calc.processPosition(childs, participants, undefined, timeFormat)
+    expect(_.isEqual(childs, childsUntouched)).to.equal(true)
+    Calc.processPosition(childs, participants, containerSize, undefined)
+    expect(_.isEqual(childs, childsUntouched)).to.equal(true)
+  })
+
+  it('should add private properties', () => {
+    const childs = clone(childsUntouched)
+    Calc.processPosition(childs, participants, containerSize, timeFormat)
+    expect(childs).to.be.an('array')
+    expect(childs.length).to.equal(4)
+    expect(_.isEqual(childs, childsUntouched)).to.equal(false)
+
+    childs.forEach(elem => {
+      expect(elem).to.have.property('_defaultEndDate')
+      expect(elem._defaultEndDate).to.be.an('date')
+
+      expect(elem).to.have.property('_width')
+      expect(elem._width).to.be.a('number')
+      expect(elem._width).to.be.above(0)
+
+      expect(elem).to.have.property('_height')
+      expect(elem._height).to.be.a('number')
+      expect(elem._height).to.be.above(0)
+
+      expect(elem).to.have.property('_position')
+      expect(elem._position).to.have.property('x')
+      expect(elem._position.x).to.be.a('number')
+      expect(elem._position.x).to.be.above(0)
+
+      expect(elem._position).to.have.property('y')
+      expect(elem._position.y).to.be.a('number')
+      expect(elem._position.y).to.be.above(0)
+    })
+  })
+
+  it('should calculate position x', () => {
+    const childs = clone(childsUntouched)
+    Calc.processPosition(childs, participants, containerSize, timeFormat)
+    const colWidth = Calc.columnSize(containerSize, participants)
+
+    childs.forEach(elem => {
+      expect(elem).to.have.property('_position')
+      expect(elem._position).to.have.property('x')
+      expect(elem._position.x).to.be.a('number')
+      expect(elem._position.x).to.be.above(0)
+      let laneNumber = participants.indexOf(elem.initiator) + 1
+      let x = Math.floor((colWidth * laneNumber) - (colWidth / 2) - (elem._width / 2))
+      expect(elem._position.x).to.equal(x)
+    })
+  })
+
+  it('should calculate position y', () => {
+    const childs = clone(childsUntouched)
+    Calc.processPosition(childs, participants, containerSize, timeFormat)
+    let startProcess = Calc.getStartProcess(childs)
+
+    childs.forEach(elem => {
+      expect(elem.start).to.be.an('date')
+      expect(startProcess.start).to.be.an('date')
+
+      expect(elem).to.have.property('_position')
+      expect(elem._position).to.have.property('y')
+      expect(elem._position.x).to.be.a('number')
+      expect(elem._position.x).to.be.above(0)
+
+      let divider = Calc.getDivider(timeFormat)
+      expect(divider).to.not.equal(0)
+      let deltaStart = elem.start - startProcess.start
+      expect(deltaStart).to.be.a('number')
+      let deltaTime = deltaStart / divider
+      let y = Math.ceil(deltaTime * Calc.timeSlice) + Calc.axisOffset
+      expect(elem._position.y).to.equal(y)
+    })
+  })
+
+  it('should calculate height without endDate', () => {
+    const participants = ['1', '2', '3']
+    const childs = [
+      new Process('1', participants[0], new Date(2017, 0, 1))
+    ]
+
+    Calc.processPosition(childs, participants, containerSize, timeFormat)
+
+    let elem = childs[0]
+    expect(elem._height).to.equal(Calc.timeSlice)
+  })
+
+  it('should calculate height with endDate', () => {
+    const participants = ['1', '2', '3']
+    const childs = [
+      new Process('1', participants[1], new Date(2017, 0, 4), new Date(2017, 0, 5)),
+      new Process('1', participants[2], new Date(2017, 1, 4), new Date(2017, 1, 6))
+    ]
+
+    Calc.processPosition(childs, participants, containerSize, timeFormat)
+
+    let elem
+
+    elem = childs[0]
+    expect(elem._height).to.equal(Calc.timeSlice)
+
+    elem = childs[1]
+    expect(elem._height).to.equal(Calc.timeSlice * 2)
+  })
+})
