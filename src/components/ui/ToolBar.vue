dbf631514f501cddf6828d59e5970988192ed31c
ajcool <jens.auerswald@gmail.com>
2017-10-09 09:30:01 +0200

    Entfernen des Zurück-Buttons -> jetzt über home

--- src/components/ui/ToolBar.vue
+++ src/components/ui/ToolBar.vue
@@ -1,35 +1,33 @@
 <template>
   <div class="tool-bar">
     <md-toolbar class="md-dense">
-      <h2 class="md-title" style="flex: 1">Prozess-Modellierung</h2>
+      <span class="span-home">
+        <md-button @touchstart.native="list" @mousedown.native="list">Prozess-Modellierung</md-button>
+      </span>
 
       <breadcrumbs :process="process" v-on:changeProcess="onChangeProcess" v-on:showProcess="onShowProcess"></breadcrumbs>
 
+      <span class="span-toolset">
+        <md-button v-if="isSaved" @touchstart.native="remove" @mousedown.native="remove">
+          <md-icon>delete</md-icon>
+        </md-button>
 
-      <md-button @touchstart.native="list" @mousedown.native="list">
-        <md-icon>arrow_back</md-icon>
-      </md-button>
+        <md-button @touchstart.native="save"  @mousedown.native="save">
+          <md-icon>save</md-icon>
+        </md-button>
 
-      <md-button v-if="isSaved" @touchstart.native="remove" @mousedown.native="remove">
-        <md-icon>delete</md-icon>
-      </md-button>
+        <md-button @touchstart.native="download"  @mousedown.native="download">
+          <md-icon>file_download</md-icon>
+        </md-button>
 
-      <md-button @touchstart.native="save"  @mousedown.native="save">
-        <md-icon>save</md-icon>
-      </md-button>
+        <md-button @touchstart.native="zoomIn" @touchend.native="zoomStop" @mousedown.native="zoomIn" @mouseup.native="zoomStop">
+          <md-icon>zoom_in</md-icon>
+        </md-button>
 
-      <md-button @touchstart.native="download"  @mousedown.native="download">
-        <md-icon>file_download</md-icon>
-      </md-button>
-
-      <md-button @touchstart.native="zoomIn" @touchend.native="zoomStop" @mousedown.native="zoomIn" @mouseup.native="zoomStop">
-        <md-icon>zoom_in</md-icon>
-      </md-button>
-
-      <md-button @touchstart.native="zoomOut" @touchend.native="zoomStop" @mousedown.native="zoomOut" @mouseup.native="zoomStop">
-        <md-icon>zoom_out</md-icon>
-      </md-button>
-
+        <md-button @touchstart.native="zoomOut" @touchend.native="zoomStop" @mousedown.native="zoomOut" @mouseup.native="zoomStop">
+          <md-icon>zoom_out</md-icon>
+        </md-button>
+      </span>
     </md-toolbar>
   </div>
 </template>
@@ -132,7 +130,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    .tool-bar {
+    .tool-bar .md-toolbar {
         white-space: nowrap;
+        display: flex;
+        justify-content: space-between;
     }
 </style>
